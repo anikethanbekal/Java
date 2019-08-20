dd5345dae534a7ed3d1fea0e45948a0edcc40368
Anikethan bekal <bekal@LAPTOP-U97JC2RM>
2019-08-20 10:23:31 +0530

    Patterns

---
+++ My_Java_Project/src/com/assignment/Pattern34.java
@@ -0,0 +1,30 @@
+/*1 2 3 4 5
+ *  6 7 8 9
+ *    0 1 2
+ *      3 4
+ *        5
+*/
+package com.assignment;
+
+public class Pattern34{
+
+	public static void main(String[] args) {
+		int k =1;
+		int n =5;
+		for (int i =0;i<n;i++)
+		{
+			for (int j =0;j<n;j++)
+			{
+				if (i<=j) 
+					System.out.print(k++%10);
+				else
+					System.out.print(" ");
+			}
+		
+			System.out.println();
+			
+		}
+
+	}
+
+}
---
+++ My_Java_Project/src/com/assignment/Pattern36.java
@@ -0,0 +1,28 @@
+/*1 2 3 4 5
+ *6 7 8 9
+ *0 1 2 
+ *3 4
+ *5
+*/
+package com.assignment;
+
+public class Pattern36{
+
+	public static void main(String[] args) {
+		int k =1;
+		int n =5;
+		for (int i =0;i<n;i++)
+		{
+			for (int j =0;j<n;j++)
+			{
+				if (i+j<=(n-1)) 
+					System.out.print(k++%10);
+			}
+		
+			System.out.println();
+			
+		}
+
+	}
+
+}
